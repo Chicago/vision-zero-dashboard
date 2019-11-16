b098a8a69de0a0fa694b9ee8c5947dd276e11e29
Chase Clark <cclark42@uic.edu>
2019-11-16 13:22:06 -0600

    data loaders

--- package/visionZero/R/utils.R
+++ package/visionZero/R/utils.R
@@ -18,8 +18,8 @@ get_data_dir <- function(){
 load_crash_data <- function(){
 
   a <- file.path(get_data_dir(),
-            "data",
-            "COMBINED CRASH DATA.Rds")
+                 "data",
+                 "COMBINED CRASH DATA.Rds")
   readRDS(a)
 
 }
@@ -32,10 +32,10 @@ load_crash_data <- function(){
 #'
 load_person_data <- function(){
 
- a <- file.path(get_data_dir(),
-            "data",
-            "COMBINED PERSON DATA.Rds")
- readRDS(a)
+  a <- file.path(get_data_dir(),
+                 "data",
+                 "COMBINED PERSON DATA.Rds")
+  readRDS(a)
 }
 
 
@@ -47,8 +47,78 @@ load_person_data <- function(){
 load_vehicle_data <- function(){
 
   a <- file.path(get_data_dir(),
-            "data",
-            "COMBINED VEHICLE DATA.Rds")
+                 "data",
+                 "COMBINED VEHICLE DATA.Rds")
   readRDS(a)
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' Load tract data
+#'
+#' @return data.table?
+#' @export
+#'
+load_tract_data <- function(){
+
+  a <- file.path(system.file("data-cook-shape",
+                             package = "visionZero"),
+                 "cook_county_census_tract_shapefile.Rds")
+  readRDS(a)
+
+}
+
+#' Load CommunityAreas data
+#'
+#' @return data.table?
+#' @export
+#'
+load_community_areas_data <- function(){
+
+  a <- file.path(system.file("data-cook-shape",
+                             package = "visionZero"),
+                 "CommunityAreas.Rds")
+  readRDS(a)
+
+}
+
+#' Load acs income data
+#'
+#' @return data.table?
+#' @export
+#'
+load_acs_income_data <- function(){
+
+  a <- file.path(system.file("data-acs",
+                             package = "visionZero"),
+                 "acs_income_2016.Rds")
+  readRDS(a)
+
+}
+
+#' Load acs population data
+#'
+#' @return data.table?
+#' @export
+#'
+load_acs_population_data <- function(){
+
+  a <- file.path(system.file("data-acs",
+                             package = "visionZero"),
+                 "acs_population_2016.Rds")
+  readRDS(a)
+
+}
+
